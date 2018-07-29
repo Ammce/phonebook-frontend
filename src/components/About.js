2a79c163f05268dd0f5c90428de950eacf4a277e
Amel Muminovic <ammce@Amels-MacBook-Pro.local>
2018-07-29 08:31:02 +0200

    Changed a small typo

--- src/components/About.js
+++ src/components/About.js
@@ -19,7 +19,7 @@ const About = () => {
             </PageHeader>
             <Alert bsStyle="success">
                 This application is running on Heroku free server, so it will take around 30 seconds to run the server because server is going to 'sleep' after every 30 minutes of inactivity.
-                <p><a style={aStyle} target='_blank' href="https://github.com/Ammce/phonebook-frontendm"> FrontEnd GitHub </a> / <a style={aStyle} target='_blank' href="https://github.com/Ammce/phonebookapi"> BackEnd GitHub </a></p>
+                <p><a style={aStyle} target='_blank' href="https://github.com/Ammce/phonebook-frontend"> FrontEnd GitHub </a> / <a style={aStyle} target='_blank' href="https://github.com/Ammce/phonebookapi"> BackEnd GitHub </a></p>
             </Alert>
         </div>
     );
